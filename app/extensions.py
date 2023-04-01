c61f086d9c6add50a9ef4b607410d9fa6a91f6fe
forva <forvantar@gmail.com>
2023-04-01 12:19:24 +0300

    New bootstrap

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,9 @@
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 
 login = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
-bootstrap = Bootstrap()
+bootstrap = Bootstrap5()
