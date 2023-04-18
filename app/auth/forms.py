f62bbd0b8c8ac635b7cddfbe2f9bb82ac9c2269e
forva <forvantar@gmail.com>
2023-04-19 01:38:23 +0300

    Remember now always true

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,6 @@ from flask_babel import lazy_gettext as _l
 class LoginForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
     password = PasswordField(_l('Password'), validators=[DataRequired()])
-    remember_me = BooleanField(_l('Remember Me'))
     submit = SubmitField(_l('Sign In'))
 
 
