da8c0fb2ace6fbbb88cd4a492cc33ab26df32e24
forva <forvantar@gmail.com>
2023-05-12 21:53:01 +0300

    Fix error

--- queue_app.py
+++ queue_app.py
@@ -1,14 +1,6 @@
 from app import create_app, db
-from app.queue.models import Queue, UserQueue
-from app.auth.models import User
 
 application = create_app()
 
 if __name__ == "__main__":
     application.run('0.0.0.0')
-
-
-    @application.shell_context_processor
-    def make_shell_context():
-        create_app()
-        return {'db': db, 'User': User, 'Queue': Queue, 'UserQueue': UserQueue}
